a99674836c2bca4885a62a7820306920624b32ea
Matthew <maa92498@gmail.com>
2021-03-01 22:41:04 -0500

    Added References
    Added references and base project setup

---
+++ ValheimMod/ValheimMod/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValheimMod
+{
+    public class Class1
+    {
+    }
+}
